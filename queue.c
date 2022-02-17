681d42f5eb0df53f5ef744e3d0d8d8b9e3fcdfc9
John Chen <chunichen@protonmail.com>
2022-02-17 22:06:51 +0800

    Add q_remove_head() in queue.c - remove element from head of queue and copy the string to sp. - use list_empty, list_first_entry, list_del_init

--- queue.c
+++ queue.c
@@ -108,7 +108,17 @@ bool q_insert_tail(struct list_head *head, char *s)
  */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *remove = list_first_entry(head, element_t, list);
+    list_del_init(head->next);
+
+    if (sp) {
+        memset(sp, '\0', bufsize);                  // plus a null terminator
+        strncpy(sp, remove->value, (bufsize - 1));  // maximum of bufsize-1
+    }
+    return remove;
 }
 
 /*
